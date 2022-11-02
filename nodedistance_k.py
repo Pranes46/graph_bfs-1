4825a669f50b5084ae2e13c5eb3f7f3cbd83841a
Pranes46 <92832458+Pranes46@users.noreply.github.com>
2022-11-01 20:11:14 -0400

    Update nodedistance_k.py

--- nodedistance_k.py
+++ nodedistance_k.py
@@ -5,30 +5,30 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution:  #tc - o(n(. sc - o(n)
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
 
         
-        adj = defaultdict(list)
+        adj = defaultdict(list). #adj list 
         
-        q = deque([root])
-        visited = set()
+        q = deque([root]). #appending the root in the queue, we are using bfs here
+        visited = set() #we are creating a hashset to store the visited node
         
-        while q:
-            curr = q.popleft()
+        while q:  #the loop will run until there is a value in queue
+            curr = q.popleft() #popping the first element in the queue
             
-            if curr.left:
+            if curr.left:  #if the popped node has left child we are appending it in the queue and we are storing the popped element as key and the left child as value in adj list
                 adj[curr.left].append(curr)
                 adj[curr].append(curr.left)
                 q.append(curr.left)
                 
-            if curr.right:
+            if curr.right: #if the popped node has right child we are appending it in the queue and we are storing the popped element as key and the right child as value in adj list
                 adj[curr.right].append(curr)
                 adj[curr].append(curr.right)
                 q.append(curr.right)
         
                 
-        q.append(target)
+        q.append(target). #appending the target in the queue
         
         while q and k>0:
             size = len(q)
